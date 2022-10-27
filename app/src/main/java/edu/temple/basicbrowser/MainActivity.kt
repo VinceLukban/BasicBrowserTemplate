a3586c8457463a284762bafcb40d90d43a72d7b5
VinceLukban <112022481+VinceLukban@users.noreply.github.com>
2022-10-27 12:17:06 -0400

    added items, in testing

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -6,23 +6,45 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
     lateinit var urlEditText: EditText
-    lateinit var goButton: Button
+    lateinit var goButton: ImageButton
     lateinit var webView: WebView
+    lateinit var url: String
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         urlEditText = findViewById(R.id.urlEditText)
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
+        webView.settings.setJavaScriptEnabled(true)
+
 
         // Allow your browser to intercept hyperlink clicks
-        webView.webViewClient = WebViewClient()
+        goButton.setOnClickListener()
+        {
+            url = urlEditText.text.toString()
+        }
 
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean
+            {
+                if (url != null)
+                {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webView.loadUrl(urlEditText.text.toString())
     }
-}
+    }
+
+
